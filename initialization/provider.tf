becebdbcea54a4d8d26eed80bbe804808fc91b62
Gangavarapu <meghana.gangavarapu@capgemini.com>
2025-12-29 15:13:45 +0530

    Terraform initialization

---
+++ initialization/provider.tf
@@ -0,0 +1,13 @@
+# terraform settings blocks(HCL)
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
