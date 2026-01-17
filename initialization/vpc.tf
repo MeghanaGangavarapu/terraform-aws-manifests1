2151fb6aa6d4927345377ffc5dbb90edb389fb77
MeghanaGangavarapu <gangavarapumeghana1231@gmail,com>
2026-01-17 21:24:23 +0530

    terraform commit

---
+++ initialization/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "vpc_demo" {
+    #Arguments
+    cidr_block = "10.160.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        "Environment" = "Development"
+        "Iac" = "Terraform"
+    }
+}
